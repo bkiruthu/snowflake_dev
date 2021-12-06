59200b2d0f3a0e998234df6b10da7a4e1b7a38e3
bkiruthu <bkiruthu@gmail.com>
2021-12-06 16:03:41 -0500

    new commit

--- Load_to_Blob.py
+++ Load_to_Blob.py
@@ -1,1 +1,31 @@
-new
+#!/usr/bin/env python
+
+
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+import os
+from azure.storage.blob import ContentSettings
+#Connect to Account
+CONNECT_STR= "DefaultEndpointsProtocol=https;AccountName=bgksolutions;AccountKey=y4vyQo7g+E2NC+GxkRrpewaK+F1Q4xOoTR8Q3KnnM9zGWErRJcyGabtcWiuDP0U9k54UMWPXb1yB0b8m+ac6XA==;EndpointSuffix=core.windows.net"
+block_blob_service = BlobServiceClient.from_connection_string(CONNECT_STR)
+print("connected")
+#Create  Container
+container_name="citibkiedata"
+block_blob_service.create_container(container_name)
+
+# Create a blob client using the local file name as the name for the blob
+def run_citi_files():
+    self_client = block_blob_service.get_container_client(container_name)
+    local_path='C:/Users/Ben/Documents/GIT/fury-chelsea/tripdata/azure'
+    path_remove = 'C:/Users/Ben/Documents/GIT/fury-chelsea/tripdata/'
+    for r, d, f in os.walk(local_path):
+        if f:
+            for file in f:
+                file_path_on_azure = os.path.join(r, file).replace(path_remove, "")
+                file_path_on_local = os.path.join(r, file)
+                blob_client = self_client.get_blob_client(file_path_on_azure)
+                with open(file_path_on_local, 'rb') as data:
+                    blob_client.upload_blob(data)
+
+if __name__ == '__main__':
+    run_citi_files()
+    print("**completed**")
